0f338c6597965c7ae7214d669cccb52a1bab9483
jamescowie <james@jcowie.co.uk>
2014-10-24 21:42:20 +0100

    Add grunt and bower

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        },
+        php: {
+            test: {
+                options: {
+                    keepalive: true,
+                    open: true,
+                    port: 8000,
+                    base: "web"
+                }
+            }
+        },
+        bower_concat: {
+            all: {
+                dest: 'web/build/_bower.js',
+                cssDest: 'web/build/_bower.css',
+                dependencies: {
+                    'underscore': 'jquery',
+                    'backbone': 'underscore',
+                    'jquery-mousewheel': 'jquery'
+                },
+                bowerOptions: {
+                    relative: false
+                }
+            }
+        },
+        injector: {
+            options: {
+                addRootSlash: false,
+                ignorePath: "web"
+            },
+            local_dependencies: {
+                files: {
+                    'web/index.php': ['web/build/*.js', 'web/build/*.css'],
+                }
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-bower-concat');
+    grunt.loadNpmTasks('grunt-injector');
+    grunt.loadNpmTasks('grunt-php');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify', 'bower_concat', 'injector', 'php']);
+
+};
---
+++ web/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Example</title>
+  <!-- injector:css -->
+  <link rel="stylesheet" href="build/_bower.css">
+  <!-- endinjector -->
+</head>
+<body>
+    <?php echo 'here be the content'; ?>
+  <!-- injector:js -->
+  <script src="build/_bower.js"></script>
+  <!-- endinjector -->
+</body>
+</html>
