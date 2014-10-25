78bc8b4788424c44602ef521499207c11cf62382
jamescowie <james@jcowie.co.uk>
2014-10-25 08:22:34 +0100

    Register whoops for errors

--- src/app.php
+++ src/app.php
@@ -6,9 +6,10 @@ use Silex\Provider\TwigServiceProvider;
 $app = new Application();
 
 $app->register(new TwigServiceProvider());
+$app->register(new Whoops\Provider\Silex\WhoopsServiceProvider);
 
 $app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
-            return $twig;
-        }));
+    return $twig;
+}));
 
 return $app;
