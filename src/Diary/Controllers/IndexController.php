abcdaedba80d240035e1ebf681e4161971da2494
jamescowie <james@jcowie.co.uk>
2014-10-28 20:40:08 +0000

    Load all tasks to homepage

--- src/Diary/Controllers/IndexController.php
+++ src/Diary/Controllers/IndexController.php
@@ -20,17 +20,30 @@ class IndexController
         if ($form->isvalid()) {
             $data = $form->getData();
 
-            $task = new Task();
-            $task->setTitle($data['task']);
-            $task->setStartDate();
-            $app['db.orm.em']->persist($task);
-            $app['db.orm.em']->flush();
+            $this->save($data, $app);
 
             $app['session']->getFlashBag()->add('message', 'Task saved to database');
             $app->redirect('/');
         }
 
         return $app['twig']->render('index.twig', array('form' => $form->createView()));
+    }
+
+    public function allAction(Application $app)
+    {
+         $tasks = $app['db.orm.em']->getRepository('Diary\Entity\Task');
+
+         return $app['twig']->render('tasks.twig', array('tasks' => $tasks->findAll()));
+    }
+
+
+    protected function save($data, Application $app)
+    {
+        $task = new Task();
+        $task->setTitle($data['task']);
+        $task->setStartDate();
+        $app['db.orm.em']->persist($task);
+        $app['db.orm.em']->flush();
     }
 
     public function helloAction($name)
